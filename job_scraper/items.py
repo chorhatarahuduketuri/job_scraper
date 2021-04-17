70a01cfe725ca540182e4b78abe2da8399195223
chorhatarahuduketuri <chorhatarahuduketuri@gmail.com>
2021-04-17 20:11:26 +0100

    Adding job item

--- job_scraper/items.py
+++ job_scraper/items.py
@@ -6,7 +6,18 @@
 import scrapy
 
 
-class JobScraperItem(scrapy.Item):
+class JobItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    job_name = scrapy.Field()
+    company = scrapy.Field()
+    salary = scrapy.Field()
+    location = scrapy.Field()
+    requirements = scrapy.Field()
+    employment_type = scrapy.Field()
+    industry = scrapy.Field()
+    department = scrapy.Field()
+    seniority = scrapy.Field()
+    date_posted = scrapy.Field()
+    description = scrapy.Field()
     pass

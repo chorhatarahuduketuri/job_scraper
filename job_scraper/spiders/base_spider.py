32cb16317549d930316a26382654bbbebe03e5c6
chorhatarahuduketuri <chorhatarahuduketuri@gmail.com>
2021-04-17 19:42:17 +0100

    BaseSpider improvements
    Including cookie handling in start_requests, functionality
    to visit paginated search results, and a useful default
    parse function.

--- job_scraper/spiders/base_spider.py
+++ job_scraper/spiders/base_spider.py
@@ -1,13 +1,57 @@
+from abc import abstractmethod, ABCMeta
+
 import scrapy
 
 
-class BaseSpider(scrapy.Spider):
-    name = 'name'
+class BaseSpider(scrapy.Spider, metaclass=ABCMeta):
+    name = None
     allowed_domains = ['']
     start_urls = ['']
 
     next_page_css = ''
     listing_css = ''
 
+    def start_requests(self):
+        for i, url in enumerate(self.start_urls):
+            yield scrapy.Request(
+                url=url,
+                callback=self.parse,
+                meta={'cookiejar': i}
+            )
+
+    def visit_next_page(self, response, css_text=''):
+        if css_text:
+            next_page = response.css(css_text).extract_first()
+        else:
+            return None
+        next_page_request = None
+        if next_page:
+            next_page_request = scrapy.Request(
+                url=response.urljoin(next_page),
+                callback=self.parse,
+                meta={'cookiejar': response.meta.get('cookiejar')}
+            )
+        return next_page_request
+
     def parse(self, response):
+        if self.listing_css:
+            listing_urls = response.css(self.listing_css).extract()
+        else:
+            listing_urls = []
+
+        for listing_url in listing_urls:
+            if not listing_url:
+                continue
+            yield scrapy.Request(
+                url=response.urljoin(listing_url),
+                callback=self.get_data
+            )
+        if self.next_page_css:
+            yield self.visit_next_page(
+                response=response,
+                css_text=self.next_page_css
+            )
+
+    @abstractmethod
+    def get_data(selfself, response):
         pass
